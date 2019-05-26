0eb9cc4baa38b8d3a0bdc9c8ccf9475c9e450184
global-value-0259 <josephtrettevik@gmail.com>
2019-05-26 17:39:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ function sayHiToGrandma(string) {
     return console.log("I can\'t hear you!")
   } else if (string.toUpperCase() === string) {
     return console.log("YES INDEED!")
-  } else if (string === "I love you") {
+  } else if (string === "I love you, Grandma") {
   } else {
     return
   }
